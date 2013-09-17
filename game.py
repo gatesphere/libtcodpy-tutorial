abf00d5c171fa8538ada0c06e5510b1cc4a31117
Jacob Peck <peckj@upstate.edu>
2013-09-17 13:55:00 -0400

    Finished part 2

--- game.py
+++ game.py
@@ -11,9 +11,49 @@ import libtcodpy as libtcod
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
+
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
 #@-<< definitions >>
 
 #@+others
+#@+node:peckj.20130917090235.2654: ** Object class
+class Object:
+  #@+others
+  #@+node:peckj.20130917090235.2655: *3* __init__
+  def __init__(self, x, y, char, color):
+    self.x = x
+    self.y = y
+    self.char = char
+    self.color = color
+  #@+node:peckj.20130917090235.2656: *3* move
+  def move(self, dx, dy):
+    if not map[self.x + dx][self.y + dy].blocked:
+      self.x += dx
+      self.y += dy
+  #@+node:peckj.20130917090235.2657: *3* draw
+  def draw(self):
+    libtcod.console_set_default_foreground(con, self.color)
+    libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+  #@+node:peckj.20130917090235.2658: *3* clear
+  def clear(self):
+    libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+  #@-others
+#@+node:peckj.20130917090235.2659: ** Tile class
+class Tile:
+  #@+others
+  #@+node:peckj.20130917090235.2660: *3* __init__
+  def __init__(self, blocked, block_sight = None):
+    self.blocked = blocked
+    
+    #by default, if a tile is blocked, it also blocks sight
+    if block_sight is None: block_sight = blocked
+    self.block_sight = block_sight
+  #@-others
+  
 #@+node:peckj.20130917090235.2651: ** setup
 # set custom font
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -21,12 +61,43 @@ libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | 
 # initialize the window
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'libtcod tutorial', False)
 
+# off-screen console
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
 # set FPS
 libtcod.sys_set_fps(LIMIT_FPS)
 
-# set player position
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+# game objects
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
+#@+node:peckj.20130917090235.2662: ** make_map
+# make the map
+def make_map():
+  global map
+  
+  # fill map with unblocked tiles
+  map = [[ Tile(False) for y in range(MAP_HEIGHT) ] for x in range(MAP_WIDTH) ]
+  
+  # pillars
+  map[30][22].blocked = True
+  map[30][22].block_sight = True
+  map[50][22].blocked = True
+  map[50][22].block_sight = True
+#@+node:peckj.20130917090235.2663: ** render_all
+def render_all():
+  for object in objects:
+    object.draw()
+  
+  for y in range(MAP_HEIGHT):
+    for x in range(MAP_WIDTH):
+      wall = map[x][y].block_sight
+      if wall:
+        libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET )
+      else:
+        libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET )
+  
+  libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 #@+node:peckj.20130917090235.2653: ** handle_keys
 def handle_keys():
   global playerx, playery
@@ -45,25 +116,27 @@ def handle_keys():
   
   # movement keys
   if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-    playery -= 1
+    player.move(0,-1)
     
   elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-    playery += 1
+    player.move(0,1)
 
   elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-    playerx -= 1
+    player.move(-1,0)
     
   elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-    playerx += 1
+    player.move(1,0)
 #@+node:peckj.20130917090235.2652: ** main loop
+make_map()
+
 while not libtcod.console_is_window_closed():
   # update screen
-  libtcod.console_set_default_foreground(0, libtcod.white)
-  libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+  render_all()
   libtcod.console_flush()
   
   # prevent @ trails (will be apparent on next update)
-  libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE) 
+  for object in objects:
+    object.clear()
   
   # grab keys (blocking)
   exit = handle_keys()
